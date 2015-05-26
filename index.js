0829ef6c7113c4411c7625200050d6a40e775b5f
unknown <kevin.silliman@tcs.com>
2015-05-26 14:49:36 -0400

    edweqdweqdweq

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/WebContent/pages'));
+
 
 
 app.get('/WebContent/pages', function(request, response) {
