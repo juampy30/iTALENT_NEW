a5e6509c5dc75244c8f09011be7190f84faaf851
unknown <kevin.silliman@tcs.com>
2015-05-26 13:28:04 -0400

    efreferferferwfer

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/WebContent/pages'));
 
-app.get('/', function(request, response) {
-  response.send('index.html');
-});
+
 app.get('/', function(request, response) {
   response.send('home.html');
 });
